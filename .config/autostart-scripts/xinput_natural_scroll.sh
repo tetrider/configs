7508943af052ed8d903e623922061a2058754f96
tetrider <v.dobrynin@ispserver.com>
2019-02-27 13:28:13 +0800

    new props

--- .config/autostart-scripts/xinput_natural_scroll.sh
+++ .config/autostart-scripts/xinput_natural_scroll.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 while [[ ! $(pgrep plasmashell) ]]; do sleep 6; done
-xinput set-prop 12 284 -1 -1 -1
-xinput set-prop 13 284 -1 -1 -1
+# xinput set-prop 12 284 -1 -1 -1
+# xinput set-prop 13 284 -1 -1 -1
+xinput set-prop 9 278 -1 -1 -1
+xinput set-prop 10 278 -1 -1 -1
+xinput set-prop 15 278 -1 -1 -1
