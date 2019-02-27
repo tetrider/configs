3787270a1a4a9ba77f7ee9cbd36c9d6f70052418
tetrider <v.dobrynin@ispserver.com>
2019-02-27 13:36:01 +0800

    deleted autostart scripts

--- .config/autostart-scripts/xcape_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while [[ ! $(pgrep plasmashell) ]]; do sleep 5; done
-setxkbmap -option ctrl:nocaps
-xcape -e 'Control_L=Escape'
-
--- .config/autostart-scripts/yakuake_bg_blur_enable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-while [[ ! $(pgrep plasmashell) ]]; do sleep 8; done
-while [[ ! $(pgrep yakuake) ]]; do sleep 1; done
-xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $(xdotool search --pid $(pidof yakuake))
